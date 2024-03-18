aba7d6226465381cb71ee47d589f599ce994e676
neilsatsangi <neil.satsangi24@gmail.com>
2024-03-18 15:47:10 -0400

    a4 state and redux

--- src/Kanbas/Courses/Modules/List.tsx
+++ src/Kanbas/Courses/Modules/List.tsx
@@ -1,12 +1,30 @@
 import React, { useState } from "react";
 import "./index.css";
 import { modules } from "../../Database";
-import { FaEllipsisV, FaCheckCircle, FaPlusCircle } from "react-icons/fa";
+import { FaEllipsisV, FaCheckCircle, FaPlusCircle, FaTrash } from "react-icons/fa";
 import { useParams } from "react-router";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addModule,
+  deleteModule,
+  updateModule,
+  setModule,
+} from "./reducer";
+import { KanbasState } from "../../store";
+
 function ModuleList() {
  const { courseId } = useParams();
- const modulesList = modules.filter((module) => module.course === courseId);
- const [selectedModule, setSelectedModule] = useState(modulesList[0]);
+ const modulesList = useSelector((state: KanbasState) => 
+    state.modulesReducer.modules);
+  const module = useSelector((state: KanbasState) => 
+    state.modulesReducer.module);
+  const dispatch = useDispatch();
+  const [selectedModule, setSelectedModule] = useState(modulesList[0]);
+
+
+
+
+
  return (
    <>
      {/* <!-- Add buttons here --> */}
@@ -29,31 +47,98 @@ function ModuleList() {
     <hr></hr>
 
 
-
-
-
-
-
-
-
-
      <ul className="list-group wd-modules">
-       {modulesList.map((module, index) => (
+     <li className="list-group-item" style={{ display: "flex", alignItems: "center" }}>
+     <button 
+onClick={() => dispatch(addModule({ ...module, course: courseId }))}     style={{ backgroundColor: "green", color: "white", borderRadius: "5px", padding: "5px 10px", border: "none", cursor: "pointer" }}>
+    Add
+</button>
+
+<button 
+onClick={() => dispatch(updateModule(module))}
+style={{ backgroundColor: "lightblue", color: "white", borderRadius: "5px", padding: "5px 10px", border: "none", cursor: "pointer" }}>
+  Update
+</button>
+
+
+
+
+  <input 
+    value={module.name}
+    onChange={(e) => 
+      dispatch(setModule({ ...module, name: e.target.value }))}
+    style={{ 
+      marginLeft: "10px", 
+      marginRight: "10px",
+      padding: "5px",
+      borderRadius: "5px",
+      border: "1px solid #ccc"
+    }}
+  />
+
+  <input
+    value={module.description}
+    onChange={(e) => 
+      dispatch(setModule({ ...module, description: e.target.value }))}
+    style={{ 
+      marginLeft: "10px",
+      padding: "5px",
+      borderRadius: "5px",
+      border: "1px solid #ccc"
+    }}
+  />
+</li>
+
+
+
+       {modulesList
+       .filter((module) => module.course === courseId)
+       .map((module, index) => (
          <li key={index}
            className="list-group-item"
            onClick={() => setSelectedModule(module)}>
            <div>
+           
+
              <FaEllipsisV className="me-2" />
-             {module.name}
+             {module.name} - {module._id}
              <span className="float-end">
                <FaCheckCircle className="text-success" />
                <FaPlusCircle className="ms-2" />
                <FaEllipsisV className="ms-2" />
+
+
+               <button
+              onClick={() => dispatch(setModule(module))}
+              style={{
+                backgroundColor: "#ADD8E6",
+                color: "black",
+                fontSize: "0.7em",
+                marginRight: "3px"
+              }}>
+              Edit
+            </button>
+
+               <button
+  onClick={() => dispatch(deleteModule(module._id))}
+  style={{
+    backgroundColor: "red",
+    color: "white",
+    fontSize: "0.7em"
+  }}
+>
+  Delete <FaTrash style={{ marginLeft: "5px" }} />
+</button>
+
+
+
              </span>
            </div>
+           <p>{module.description}</p> 
+           
            {selectedModule._id === module._id && (
              <ul className="list-group">
-               {module.lessons?.map((lesson, index) => (
+               {module.lessons?.map((lesson: any, index: any) => (
                  <li className="list-group-item" key={index}>
                    <FaEllipsisV className="me-2" />
                    {lesson.name}
@@ -61,7 +146,10 @@ function ModuleList() {
                      <FaCheckCircle className="text-success" />
                      <FaEllipsisV className="ms-2" />
                    </span>
+                   <p>{lesson.description}</p> 
+                   
                  </li>
+
                ))}
              </ul>
            )}
--- src/Kanbas/Dashboard/index.tsx
+++ src/Kanbas/Dashboard/index.tsx
@@ -1,19 +1,51 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
-import { courses } from "../Database";
-function Dashboard() {
-  const courseCount = courses.length;
+import { courses as courseData } from "../Database"; 
 
+function Dashboard({
+   courses, course, setCourse, addNewCourse,
+    deleteCourse, updateCourse }: {
+    courses: any[]; course: any; setCourse: (course: any) => void;
+    addNewCourse: () => void; deleteCourse: (course: any) => void;
+    updateCourse: () => void; })
+   {
+  
   return (
     <div className="p-4">
       <h1>Dashboard</h1>
       <hr />
-      <h2>Published Courses ({courseCount})</h2>
+      <h2>Published Courses ({courses.length})</h2>
       <hr />
+
+      <h5>Course</h5>
+      <input value={course.name} className="form-control"
+             onChange={(e) => setCourse({ ...course, name: e.target.value }) } />
+      <input value={course.number} className="form-control"
+             onChange={(e) => setCourse({ ...course, number: e.target.value }) } />
+      <input value={course.startDate} className="form-control" type="date"
+             onChange={(e) => setCourse({ ...course, startDate: e.target.value }) }/>
+      <input value={course.endDate} className="form-control" type="date"
+             onChange={(e) => setCourse({ ...course, endDate: e.target.value }) } />
+
+
+<button onClick={addNewCourse} style={{ backgroundColor: "green", color: "white", margin: "5px" }}>
+  Add
+</button>
+
+
+      <button onClick={updateCourse} style={{ backgroundColor: "#6495ED", color: "white", margin: "5px" }}>
+  Update
+</button>
+
+
+
+
       <div className="row">
         <div className="row row-cols-1 row-cols-md-5 g-4">
           {courses.map((course) => (
             <div key={course._id} className="col" style={{ width: 300 }}>
+              
+
               <div className="card">
                 <img
                   src={`/images/${course.image}`}
@@ -34,12 +66,21 @@ function Dashboard() {
                     {course.name}{" "}
                   </Link>
                   <p className="card-text">{course.description}</p>
-                  <Link
-                    to={`/Kanbas/Courses/${course._id}/Home`}
-                    className="btn btn-primary"
-                  >
-                    Go
-                  </Link>
+
+                  <button onClick={(event) => {
+                        event.preventDefault();
+                        deleteCourse(course._id);
+                      }} style={{ backgroundColor: "red", color: "white", margin: "5px" }}>
+                      Delete
+              </button>
+
+              <button onClick={(event) => {
+                event.preventDefault();
+                setCourse(course);
+              }} style={{ backgroundColor: "violet", color: "white", margin: "5px" }}>
+              Edit
+            </button>
+
                 </div>
               </div>
             </div>
